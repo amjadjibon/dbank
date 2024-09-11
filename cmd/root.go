629e46967beaaf4b8cf53934c43d2f221ae1865c
Amjad Jibon <amjad.jibon@gmail.com>
2024-09-11 19:39:58 +0800

    cobra cli init

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "dbank",
+	Short: "Digital Bank CLI tool to manage migrations, seeds, server and more",
+	Long: `Digital Bank CLI tool to manage migrations, seeds, server and more
+Complete documentation is available at https://github.com/amjadjibon/dbank`,
+	Run: func(cmd *cobra.Command, args []string) {
+		_ = cmd.Help()
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// TODO: Add subcommands
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/amjadjibon/dbank/cmd"
+
+func main() {
+	cmd.Execute()
+}
